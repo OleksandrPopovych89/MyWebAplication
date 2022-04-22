941dfcb3ecb55c015cd6a10ed933fccfa727529f
Oleksndr <a.popovych.89@gmail.com>
2022-04-22 22:27:11 +0300

    Update

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -1,14 +1,17 @@
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "Servlet", value = "/Servlet")
 public class FirstServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1>Hello world</h1>");
+        pw.println("</html>");
     }
 
     @Override
