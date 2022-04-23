60aa61d45044d9f1e04a5588d975c2ef0fc0425f
Oleksndr <a.popovych.89@gmail.com>
2022-04-23 17:40:19 +0300

    Update

--- src/main/java/com/gmail/oleksandrpopovych89/servlets/FirstServlet.java
+++ src/main/java/com/gmail/oleksandrpopovych89/servlets/FirstServlet.java
@@ -1,21 +1,22 @@
+package com.gmail.oleksandrpopovych89.servlets;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class FirstServlet extends HttpServlet {
+    private static String index = "/WEB-INF/view/firstJsp.jsp";
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter pw = response.getWriter();
-        pw.println("<html>");
-        pw.println("<h1>Hello world</h1>");
-        pw.println("</html>");
+        request.getRequestDispatcher(index).forward(request, response);
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
     }
 }
+
